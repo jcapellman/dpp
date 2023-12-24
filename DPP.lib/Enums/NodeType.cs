f1e92e6ed5ac8dadc8ad99f70758d15f53b6003a
Jarred Capellman <jcapellman@hotmail.com>
2023-12-24 07:51:12 -0600

    Added: NodeType

---
+++ DPP.lib/Enums/NodeType.cs
@@ -0,0 +1,10 @@
+﻿namespace DPP.lib.Enums
+{
+    [Flags]
+    public enum NodeType
+    {
+        ALL = 0,
+        DATA = 1,
+        TRANSFORMER = 2
+    }
+}
--- DPP.lib/Nodes/Base/BaseNode.cs
+++ DPP.lib/Nodes/Base/BaseNode.cs
@@ -1,8 +1,14 @@
-﻿namespace DPP.lib.Nodes.Base
+﻿using DPP.lib.Enums;
+
+namespace DPP.lib.Nodes.Base
 {
     public abstract class BaseNode
     {
         public abstract string Name { get; }
+
+        public abstract NodeType AllowedInputs { get; }
+
+        public abstract NodeType AllowedOutputs { get; }
 
         public abstract object Process(object input);
     }
